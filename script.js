287d9fdd09496de2645348362c78169fd71b7cc2
Vikii-5 <72004386+Vikii-5@users.noreply.github.com>
2022-11-26 21:41:53 +0530

    validation function altered

--- script.js
+++ script.js
@@ -1,15 +1,55 @@
-function genarateQuiz() {
-  let amount = document.getElementById("amount");
-  let category = document.getElementById("category");
-  let difficulty = document.getElementById("difficulty");
-  let type = document.getElementById("type");
+let generateBtn = document.getElementById("generate");
 
+generateBtn.addEventListener("click", function (event) {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".needs-validation");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach((form) => {
+    form.addEventListener(
+      "submit",
+      (event) => {
+        if (!form.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        } else {
+          let amount = document.getElementById("amount");
+          let category = document.getElementById("category");
+          let difficulty = document.getElementById("difficulty");
+          let type = document.getElementById("type");
+
+          let baseURL = `https://opentdb.com/api.php?`;
+
+          if (amount.value !== "") {
+            baseURL += `${amount.name}=${amount.value}&`;
+            return baseURL;
+          }
+
+          if (category.value !== "") {
+            baseURL += `${category.name}=${category.value}&`;
+            return baseURL;
+          }
+
+          if (difficulty.value !== "") {
+            baseURL += `${difficulty.name}=${difficulty.value}&`;
+            return baseURL;
+          }
+
+          if (type.value !== "") {
+            baseURL += `${type.name}=${type.value}&`;
+            return baseURL;
+          }
+
+          console.log(baseURL.slice(0, -1));
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
   
+});
 
-  let URL = `https://opentdb.com/api.php?${amount.name}=${amount.value}&${category.name}=${category.value}&${difficulty.name}=${difficulty.value}&${type.name}=${type.value}`;
-
-  console.log(URL);
-}
-
-let generateBtn = document.getElementById('generate');
-generateBtn.addEventListener('click', genarateQuiz);
